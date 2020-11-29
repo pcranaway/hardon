55e24a1c3e641d99a5adbe10d7e3c10e2c729452
pcranaway <pcranaway@tuta.io>
2020-11-29 20:58:31 +0200

    add soup healing

--- src/main/java/top/retarders/hardon/event/warzone/WarzoneListener.java
+++ src/main/java/top/retarders/hardon/event/warzone/WarzoneListener.java
@@ -4,7 +4,10 @@ import me.lucko.helper.Events;
 import me.lucko.helper.Helper;
 import me.lucko.helper.terminable.TerminableConsumer;
 import me.lucko.helper.terminable.module.TerminableModule;
+import org.bukkit.Material;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import top.retarders.hardon.event.warzone.handler.DeathHandler;
 import top.retarders.hardon.user.repo.UserRepository;
 import top.retarders.hardon.user.state.UserState;
@@ -19,5 +22,11 @@ public class WarzoneListener implements TerminableModule {
                 .filter(event -> this.repository.find(event.getEntity().getUniqueId()).get().state == UserState.WARZONE)
                 .handler(new DeathHandler())
                 .bindWith(consumer);
+
+        Events.subscribe(PlayerInteractEvent.class)
+                .filter(event -> event.getItem() != null)
+                .filter(event -> event.getItem().getType() == Material.MUSHROOM_SOUP)
+                .filter(event -> event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK)
+                .handler(event -> event.getPlayer().setHealth(event.getPlayer().getMaxHealth() + 3.5));
     }
 }
